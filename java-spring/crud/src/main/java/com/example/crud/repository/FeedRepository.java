5aafd5a035bbca2f46b6cb6047722eb994fbe0f3
gilgeunwoo <kesksjxhx@gmail.com>
2023-03-02 09:12:58 +0900

    쿼리함수 추가

--- java-spring/crud/src/main/java/com/example/crud/repository/FeedRepository.java
+++ java-spring/crud/src/main/java/com/example/crud/repository/FeedRepository.java
@@ -3,5 +3,9 @@ package com.example.crud.repository;
 import com.example.crud.entity.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FeedRepository extends JpaRepository<Feed, Long> {
+
+    List<Feed> findAllByOrderByIdDesc();
 }
