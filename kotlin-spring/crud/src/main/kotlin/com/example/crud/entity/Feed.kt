6158ef1b0b1c9748b1811ff806908beddba25bda
gilgeunwoo <kesksjxhx@gmail.com>
2023-03-03 22:59:02 +0900

    Feed 엔티티 생성

---
+++ kotlin-spring/crud/src/main/kotlin/com/example/crud/entity/Feed.kt
@@ -0,0 +1,15 @@
+package com.example.crud.entity
+
+import javax.persistence.*
+
+@Entity
+class Feed (
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private val id : Long,
+
+        @Column(columnDefinition = "VARCHAR(20)", nullable = false)
+        private val title : String,
+
+        @Column(columnDefinition = "VARCHAR(1000)", nullable = false)
+        private val content : String,
+)
