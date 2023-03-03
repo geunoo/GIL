735754828976a7b5308870ed9faa23699cb6de4c
gilgeunwoo <kesksjxhx@gmail.com>
2023-03-03 22:59:19 +0900

    FeedRepository

---
+++ kotlin-spring/crud/src/main/kotlin/com/example/crud/repository/FeedRepository.kt
@@ -0,0 +1,9 @@
+package com.example.crud.repository
+
+import com.example.crud.entity.Feed
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface FeedRepository : JpaRepository<Feed, Long> {
+
+
+}
