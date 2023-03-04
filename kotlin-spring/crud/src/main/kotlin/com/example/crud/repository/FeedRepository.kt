5622f2aacaf0ae9488e3f5694de9b7ab4fef85c9
gilgeunwoo <kesksjxhx@gmail.com>
2023-03-04 21:12:38 +0900

    쿼리함수 추가

--- kotlin-spring/crud/src/main/kotlin/com/example/crud/repository/FeedRepository.kt
+++ kotlin-spring/crud/src/main/kotlin/com/example/crud/repository/FeedRepository.kt
@@ -4,4 +4,6 @@ import com.example.crud.entity.Feed
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface FeedRepository : JpaRepository<Feed, Long> {
+
+    fun findAllByOrderByIdDesc(): List<Feed>
 }
