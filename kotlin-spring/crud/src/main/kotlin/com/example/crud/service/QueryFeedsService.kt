b00963188e6755927330d48eeb9290e1999dfa04
gilgeunwoo <kesksjxhx@gmail.com>
2023-03-05 18:05:53 +0900

    트랜젝션 처리

--- kotlin-spring/crud/src/main/kotlin/com/example/crud/service/QueryFeedsService.kt
+++ kotlin-spring/crud/src/main/kotlin/com/example/crud/service/QueryFeedsService.kt
@@ -3,12 +3,14 @@ package com.example.crud.service
 import com.example.crud.controller.dto.response.QueryFeedsResponse
 import com.example.crud.repository.FeedRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class QueryFeedsService(
         private val feedRepository: FeedRepository,
 ) {
 
+    @Transactional(readOnly = true)
     fun execute(): List<QueryFeedsResponse> {
         return feedRepository.findAllByOrderByIdDesc().stream()
                 .map {
