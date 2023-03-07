96397bebe185b1534923b00be4a339c172c08ee6
gilgeunwoo <kesksjxhx@gmail.com>
2023-03-07 17:12:50 +0900

    gradle 설정

--- kotlin-spring/feed/build.gradle.kts
+++ kotlin-spring/feed/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
     kotlin("plugin.jpa") version "1.7.22"
+    kotlin("kapt") version "1.7.22"
 }
 
 group = "com.example"
@@ -23,15 +24,33 @@ repositories {
 }
 
 dependencies {
+
+    //database
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.mysql:mysql-connector-j")
+
+    //validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    //web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    //time-based UUID generator
+    implementation("com.fasterxml.uuid:java-uuid-generator:3.1.4")
+
+    //kotlin-reflect
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    //configuration-processor
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    //test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    //querydsl
+    implementation("com.querydsl:querydsl-jpa:5.0.0")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jpa")
 }
 
 tasks.withType<KotlinCompile> {
@@ -39,6 +58,18 @@ tasks.withType<KotlinCompile> {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = "17"
     }
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+noArg {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
 }
 
 tasks.withType<Test> {
