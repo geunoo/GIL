09644c47942f2cd7eb5dbd428d728705054d0256
gilgeunwoo <kesksjxhx@gmail.com>
2023-03-07 17:14:08 +0900

    Feed entity 생성

---
+++ kotlin-spring/feed/src/main/kotlin/com/example/feed/domain/feed/domain/Feed.kt
@@ -0,0 +1,25 @@
+package com.example.feed.domain.feed.domain
+
+import com.example.feed.domain.user.domain.User
+import com.example.feed.global.entity.BaseEntity
+import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.FetchType
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+
+class Feed(
+
+        override val id: UUID,
+
+        @Column(columnDefinition = "VARCHAR(30)", nullable = false)
+        val title: String,
+
+        @Column(columnDefinition = "VARCHAR(1000)", nullable = false)
+        val content: String,
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "user_id", nullable = false)
+        val user: User,
+
+): BaseEntity()
