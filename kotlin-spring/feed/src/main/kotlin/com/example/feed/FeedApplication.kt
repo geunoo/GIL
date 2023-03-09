b791a9524023f920ccacf94bd9ca98a1fcf7243c
gilgeunwoo <kesksjxhx@gmail.com>
2023-03-09 22:43:07 +0900

    application에 어노테이션 추가

--- kotlin-spring/feed/src/main/kotlin/com/example/feed/FeedApplication.kt
+++ kotlin-spring/feed/src/main/kotlin/com/example/feed/FeedApplication.kt
@@ -1,8 +1,12 @@
 package com.example.feed
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableJpaAuditing
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class FeedApplication
 
