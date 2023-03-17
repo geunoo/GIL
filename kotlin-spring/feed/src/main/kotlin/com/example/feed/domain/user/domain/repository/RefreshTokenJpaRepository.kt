61ce61741920db12bd6247a2eb9bbad648460d54
gilgeunwoo <kesksjxhx@gmail.com>
2023-03-17 22:11:13 +0900

    쿼리 함수 추가

--- kotlin-spring/feed/src/main/kotlin/com/example/feed/domain/user/domain/repository/RefreshTokenJpaRepository.kt
+++ kotlin-spring/feed/src/main/kotlin/com/example/feed/domain/user/domain/repository/RefreshTokenJpaRepository.kt
@@ -4,4 +4,6 @@ import com.example.feed.domain.user.domain.RefreshToken
 import org.springframework.data.repository.CrudRepository
 
 interface RefreshTokenJpaRepository : CrudRepository<RefreshToken, String> {
+
+    fun queryByToken(token: String): RefreshToken?
 }
