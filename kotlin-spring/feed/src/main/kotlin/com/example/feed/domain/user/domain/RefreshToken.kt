ea9cced5208ea7009acd1992ea8c110f3276df0d
gilgeunwoo <kesksjxhx@gmail.com>
2023-03-17 22:22:48 +0900

    setter은닉

--- kotlin-spring/feed/src/main/kotlin/com/example/feed/domain/user/domain/RefreshToken.kt
+++ kotlin-spring/feed/src/main/kotlin/com/example/feed/domain/user/domain/RefreshToken.kt
@@ -10,13 +10,18 @@ class RefreshToken(
 
         @Id
         val accountId: String,
+        token: String,
+        ttl: Long
+) {
 
         @Indexed
-        var token: String,
+        var token: String = token
+                private set
 
         @TimeToLive
-        var ttl: Long
-) {
+        var ttl: Long = ttl
+                private set
+
         fun update(token: String, ttl: Long) {
                 this.token = token
                 this.ttl = ttl
