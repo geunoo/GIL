a1f40781bffe9f922771122deb12e351b2f2a5df
gilgeunwoo <kesksjxhx@gmail.com>
2023-03-21 15:14:04 +0900

    쿼리 함수

--- kotlin-spring/feed/src/main/kotlin/com/example/feed/domain/feed/domain/repository/FeedRepository.kt
+++ kotlin-spring/feed/src/main/kotlin/com/example/feed/domain/feed/domain/repository/FeedRepository.kt
@@ -1,13 +1,35 @@
 package com.example.feed.domain.feed.domain.repository
 
 import com.example.feed.domain.feed.domain.Feed
+import com.example.feed.domain.feed.domain.QFeed.feed
+import com.example.feed.domain.feed.presentation.dto.response.QQueryFeedsResponse
+import com.example.feed.domain.feed.presentation.dto.response.QueryFeedsResponse
+import com.example.feed.domain.user.domain.QUser.user
+import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.stereotype.Repository
 
 @Repository
 class FeedRepository(
     private val feedJpaRepository: FeedJpaRepository,
+    private val jpaQueryFactory: JPAQueryFactory,
 ) {
 
     fun saveFeed(feed: Feed): Feed =
         feedJpaRepository.save(feed)
+
+    fun queryFeeds(): List<QueryFeedsResponse> =
+        jpaQueryFactory
+            .select(
+                QQueryFeedsResponse(
+                    feed.title,
+                    feed.content,
+                    user.name,
+                    feed.createdAt
+                )
+            )
+            .from(feed)
+            .join(feed.user, user)
+            .orderBy(feed.createdAt.desc())
+            .fetch()
+
 }
