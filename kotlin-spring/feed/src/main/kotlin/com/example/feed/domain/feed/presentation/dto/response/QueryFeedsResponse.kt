4bb1871122227715a4b9b88c802fe1d18903b855
gilgeunwoo <kesksjxhx@gmail.com>
2023-03-21 15:13:57 +0900

    QueryProjection

--- kotlin-spring/feed/src/main/kotlin/com/example/feed/domain/feed/presentation/dto/response/QueryFeedsResponse.kt
+++ kotlin-spring/feed/src/main/kotlin/com/example/feed/domain/feed/presentation/dto/response/QueryFeedsResponse.kt
@@ -1,13 +1,15 @@
 package com.example.feed.domain.feed.presentation.dto.response
 
 import com.fasterxml.jackson.annotation.JsonFormat
+import com.querydsl.core.annotations.QueryProjection
 import java.time.LocalDateTime
 
-data class QueryFeedsResponse(
+data class QueryFeedsResponse @QueryProjection constructor(
 
     val title: String,
     val content: String,
+    val username: String,
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-mm-dd HH:MM:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss", timezone = "Asia/Seoul")
     val createdAt: LocalDateTime
 )
