39ba22f26228afc0006ec3da1d310b842d62f1e5
gilgeunwoo <kesksjxhx@gmail.com>
2023-04-29 23:49:51 +0900

    🗂️ :: hexagonal-feed 프로젝트 세팅

---
+++ java-spring/hexagonal-feed/hexagonal-feed-infrastructure/src/main/java/com/example/hexagonalfeed/HexagonalFeedApplication.java
@@ -0,0 +1,13 @@
+package com.example.hexagonalfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalFeedApplication.class, args);
+    }
+
+}
---
+++ java-spring/hexagonal-feed/hexagonal-feed-infrastructure/src/test/java/com/example/hexagonalfeed/HexagonalFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hexagonalfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
