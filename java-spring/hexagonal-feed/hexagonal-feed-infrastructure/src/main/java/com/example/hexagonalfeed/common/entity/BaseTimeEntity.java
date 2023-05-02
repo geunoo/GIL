2fd905494dffbae939184733b020f3c739e8018f
gilgeunwoo <kesksjxhx@gmail.com>
2023-05-02 23:36:51 +0900

    ⚙️ :: BaseTimeEntity 안되는 오류 수정

--- java-spring/hexagonal-feed/hexagonal-feed-infrastructure/src/main/java/com/example/hexagonalfeed/common/entity/BaseTimeEntity.java
+++ java-spring/hexagonal-feed/hexagonal-feed-infrastructure/src/main/java/com/example/hexagonalfeed/common/entity/BaseTimeEntity.java
@@ -4,18 +4,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
-@SuperBuilder
 @NoArgsConstructor
+@SuperBuilder
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class BaseTimeEntity extends BaseUUIDEntity {
 
     @CreatedDate
-    @Column(columnDefinition = "DATETIME(6)", nullable = false, updatable = false)
+    @Column(columnDefinition = "DATETIME(6)", updatable = false)
     private LocalDateTime createdAt;
 }
