b6278bca81ff733024a2a79df9dde6463300938b
gilgeunwoo <kesksjxhx@gmail.com>
2023-07-20 15:14:56 +0900

    🛠️ :: Json 객체 변환 메서드

--- java-spring/kafka/src/main/java/com/example/kafka/consumer/FeedConsumer.java
+++ java-spring/kafka/src/main/java/com/example/kafka/consumer/FeedConsumer.java
@@ -1,23 +1,20 @@
 package com.example.kafka.consumer;
 
+import com.example.kafka.JsonConverter;
 import com.example.kafka.domain.Feed;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class FeedConsumer {
 
+    private final JsonConverter<Feed> jsonConverter;
+
     @KafkaListener(topics = "feed", groupId = "bar")
-    public void consume(String feed) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            Feed feed1 = objectMapper.readValue(feed, Feed.class);
-            System.out.println(feed1.toString());
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
+    public void consume(String feedString) {
+        Feed feed = jsonConverter.convert(feedString, Feed.class);
+        System.out.println(feed.toString());
     }
 }
--- java-spring/kafka/src/main/java/com/example/kafka/consumer/UserConsumer.java
+++ java-spring/kafka/src/main/java/com/example/kafka/consumer/UserConsumer.java
@@ -1,23 +1,20 @@
 package com.example.kafka.consumer;
 
+import com.example.kafka.JsonConverter;
 import com.example.kafka.domain.User;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class UserConsumer {
 
+    private final JsonConverter<User> jsonConverter;
+
     @KafkaListener(topics = "user", groupId = "foo")
-    public void consume(String user) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            User user1 = objectMapper.readValue(user, User.class);
-            System.out.println(user1.toString());
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
+    public void consume(String userString) {
+        User user = jsonConverter.convert(userString, User.class);
+        System.out.println(user.toString());
     }
 }
