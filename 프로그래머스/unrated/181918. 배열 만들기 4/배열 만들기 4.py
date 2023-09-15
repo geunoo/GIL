4ab0b2f1a41d34283c87c991e751ca3bbcf18037
길근우 <103028187+gilgeunwoo@users.noreply.github.com>
2023-09-15 10:14:46 +0900

    [unrated] Title: 배열 만들기 4, Time: 3.78 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.py
@@ -0,0 +1,14 @@
+def solution(arr):
+    stk = []
+    i = 0
+    while i <len(arr):
+        if not stk:
+            stk.append(arr[i])
+            i += 1
+        else:
+            if stk[-1] < arr[i]:
+                stk.append(arr[i])
+                i += 1
+            else:
+                stk.pop()
+    return stk
